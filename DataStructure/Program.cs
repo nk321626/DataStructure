4404f8de2e8c5b811eb47ee0043654e9faafb9e4
nk321626 <nk321626@gmail.com>
2022-05-15 14:56:40 +0530

    Added Welocome Message

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Data Structure Problem Program!");
-Console.ReadLine();
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structure Problem Program!");
+
+            Console.WriteLine("Search Operation !");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.WriteLine("============================================");
+            Console.ReadLine();
+        }
+    }
+}
